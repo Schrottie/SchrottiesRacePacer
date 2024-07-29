e07a18081b98d695e988782f6a57f80e4cfa5dc6
Bischoff <maik.bischoff@berliner-feuerwehr.de>
2024-07-29 11:45:19 +0200

    .

--- pacer.js
+++ pacer.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const paceTableBody = document.querySelector('#paceTable tbody');
     const toggleImage = document.getElementById('toggleImage');
 
+    let data = evenWerte; // Starten mit geraden Jahren
+
     function formatTime(minutes) {
         let hours = Math.floor(minutes / 60);
         let mins = Math.floor(minutes % 60);
@@ -11,10 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function calculateTime(startTime, kilometer, pace) {
-        let timeInMinutes = startTime + (kilometer * pace);
-        if (timeInMinutes >= 1440) { // 1440 Minuten = 24 Stunden
-            timeInMinutes -= 1440;
-        }
+        const timeInMinutes = startTime + (kilometer * pace);
         return formatTime(timeInMinutes);
     }
 
@@ -37,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
             if (item.vp.includes('WP')) {
                 row.classList.add('wp-row');
-            } else if (item.vp.includes('VP 10') && toggleImage.src.includes('uzs2.png')) {
+            } else if (item.vp.includes('VP 10') && !toggleImage.src.includes('uzs1.png')) {
                 row.classList.add('vp10-row');
             }
 
@@ -46,38 +45,36 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function handleToggleChange() {
-        const values = toggleImage.src.includes('uzs2.png') ? evenWerte : oddWerte;
-        updateTable(values, paceSlider.value);
+        const isOddYear = (new Date().getFullYear() % 2 !== 0);
+        if (toggleImage.src.includes('uzs1.png')) {
+            data = oddWerte;
+        } else {
+            data = evenWerte;
+        }
+        updateTable(data, paceSlider.value);
     }
 
-    toggleImage.addEventListener('click', function() {
-        if (toggleImage.src.includes('uzs2.png')) {
-            toggleImage.src = 'uzs1.png';
-        } else {
+    function updateToggleImage() {
+        if (toggleImage.src.includes('uzs1.png')) {
             toggleImage.src = 'uzs2.png';
+        } else {
+            toggleImage.src = 'uzs1.png';
         }
         handleToggleChange();
-    });
-
-    const initialPace = 515;
-    paceSlider.max = 665; // Maximalwert auf 11:05 min/km setzen
-    paceSlider.value = initialPace;
-    paceDisplay.textContent = '8:35';
-
-    const isEvenYear = (new Date().getFullYear() % 2 === 0);
-    if (isEvenYear) {
-        toggleImage.src = 'uzs2.png';
-    } else {
-        toggleImage.src = 'uzs1.png';
     }
 
-    handleToggleChange();
+    paceSlider.max = 665; // Maximalwert auf 11:05 min/km setzen
+    paceSlider.value = 515;
+    paceDisplay.textContent = '8:35';
+    updateTable(data, paceSlider.value);
 
     paceSlider.addEventListener('input', function() {
         const pace = paceSlider.value;
         const minutes = Math.floor(pace / 60);
         const seconds = pace % 60;
         paceDisplay.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
-        handleToggleChange();
+        updateTable(data, pace);
     });
+
+    toggleImage.addEventListener('click', updateToggleImage);
 });
