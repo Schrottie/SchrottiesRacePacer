76eaea5333092fc275eaa686d4396f643d1931fe
Bischoff <maik.bischoff@berliner-feuerwehr.de>
2024-08-29 10:44:08 +0200

    Bugfix Raceliste

--- list_races.php
+++ list_races.php
@@ -10,10 +10,11 @@ foreach ($files as $file) {
         $fileContents = file_get_contents($filePath);
         $jsonData = json_decode($fileContents, true);
 
-        // Verwende den Titel aus dem JSON
+        // Verwende den Titel und das pinned-Flag aus dem JSON
         $raceFiles[] = array(
             'filename' => $file,
-            'fullName' => isset($jsonData['title']) ? $jsonData['title'] : pathinfo($file, PATHINFO_FILENAME)
+            'fullName' => isset($jsonData['title']) ? $jsonData['title'] : pathinfo($file, PATHINFO_FILENAME),
+            'pinned' => isset($jsonData['pinned']) ? $jsonData['pinned'] : false // Hinzufügen des pinned-Flags
         );
     }
 }
